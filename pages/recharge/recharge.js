7ffb7eb6339c16657e0c15a25850bf8d399746d4
wuyanxingze <526899331@qq.com>
2018-08-24 15:09:50 +0800

    修改充值页面2

--- pages/recharge/recharge.js
+++ pages/recharge/recharge.js
@@ -6,7 +6,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-    formatedNum: ""
+    formatedNum: "",
+    disable: false
   },
   /**
    * 生命周期函数--监听页面加载
@@ -15,24 +16,9 @@ Page({
     let that = this;
     server.getJSON('/Recharge/getRechargeRulesList', function(res) {
       let rules = res.data;
-      if (rules.length > 0) {
-        let arr = new Set();
-        rules.forEach(function(val) {
-          arr.add(val.lower_limit_money)
-        })
-        that.setData({
-          rules,
-          selectlists: Array.from(arr)
-        })
-      }
-    })
-  },
-  bindCheckbox(e) {
-    let selectedindex = e.currentTarget.dataset.index;
-    let formatedNum = this.data.selectlists[selectedindex];
-    this.setData({
-      selectedindex,
-      formatedNum
+      that.setData({
+        rules
+      })
     })
   },
   /**
@@ -42,8 +28,7 @@ Page({
     let that = this;
     let count = e.detail.value;
     if (/^[0-9]+(.[0-9]{1,2})?$/.exec(count)) {
-      let selectlists = this.data.selectlists;
-      let selectedindex = 'none';
+      
       // if (!(parseFloat(count) >= 1)) {
       //   wx.showToast({
       //     title: '充值金额需要大于1元',
@@ -53,15 +38,8 @@ Page({
       //     formatedNum: ""
       //   })
       // } else {
-      selectlists.forEach(function(val, index) {
-        if (val == count) {
-          console.log(val, count)
-          selectedindex = index;
-        }
-      })
       that.setData({
-        formatedNum: count,
-        selectedindex
+        formatedNum: count
       })
       // }
     } else {
@@ -73,38 +51,53 @@ Page({
   },
   // 充值
   preparepay(rechargeAmount) {
-    server.newpostJSON('/Recharge/prepare_pay', {
-      openid: app.globalData.openid,
-      total_amount: rechargeAmount
-    }, function(res) {
-      wx.requestPayment({
-        'timeStamp': res.data.data.timeStamp,
-        'nonceStr': res.data.data.nonceStr,
-        'package': res.data.data.package,
-        'signType': res.data.data.signType,
-        'paySign': res.data.data.paySign,
-        'success': function(res) {
-          wx.showToast({
-            title: '充值成功',
-            icon: 'success',
-            duration: 2000,
-            complete: function() {
-              setTimeout(function() {
-                wx.navigateBack()
-              }, 1500)
-            }
-          })
-
-        },
-        'fail': function(res) {
-          wx.showToast({
-            title: '充值失败',
-            image: '/images/about.png',
-            duration: 2000,
-          })
-        }
+    let that =this;
+    if (!this.data.disable) {
+      // 点击先把点击禁用  支付结束在开启
+      this.setData({
+        disable: true
       })
-    })
+      server.newpostJSON('/Recharge/prepare_pay', {
+        openid: app.globalData.openid,
+        total_amount: rechargeAmount
+      }, function(res) {
+        wx.requestPayment({
+          'timeStamp': res.data.data.timeStamp,
+          'nonceStr': res.data.data.nonceStr,
+          'package': res.data.data.package,
+          'signType': res.data.data.signType,
+          'paySign': res.data.data.paySign,
+          'success': function(res) {
+            that.setData({
+              disable: false
+            })
+            wx.showToast({
+              title: '充值成功',
+              icon: 'success',
+              duration: 2000,
+              complete: function() {
+                setTimeout(function() {
+                  wx.navigateBack()
+                }, 1500)
+              }
+            })
+
+          },
+          'fail': function(res) {
+            that.setData({
+              disable: false
+            })
+            wx.showToast({
+              title: '充值失败',
+              image: '/images/about.png',
+              duration: 2000,
+            })
+          }
+        })
+      })
+    } else {
+      return;
+    }
   },
   // 点击微信充值
   wxRecharge: function() {
@@ -123,16 +116,15 @@ Page({
   },
   // 选择固定充值金额 该事件绑定在父元素
   rechargeTap(e) {
-    // console.log(e.target.dataset);
-    let rechargeAmount = 0.01;
-    if (!isNaN(+rechargeAmount) && rechargeAmount){
-      this.preparepay(+rechargeAmount)
-    }else{
-      wx.showToast({
-        title: '充值的金额有误',
-        image: '/images/about.png',
-        duration:2000
-      })
-    }
+      let rechargeAmount = e.target.dataset.amount;
+      if (!isNaN(+rechargeAmount) && rechargeAmount) {
+        this.preparepay(+rechargeAmount)
+      } else {
+        wx.showToast({
+          title: '充值的金额有误',
+          image: '/images/about.png',
+          duration: 2000
+        })
+      }
   }
 })
